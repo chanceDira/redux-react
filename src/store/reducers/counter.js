64b44941d81c8d56675217145323383ed7f90ae1
chanceDira <chancedesire60@gmail.com>
2021-10-01 13:06:09 +0200

    Middleware test

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,34 +1,34 @@
-import * as actionTypes from '../actions/actions'
+// import * as actionTypes from '../actions/actions'
 
-const initialState = {
-    counter: 0
-}
+// const initialState = {
+//     counter: 0
+// }
 
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case actionTypes.INCREMENT:
-            const newState = Object.assign({}, state)
-            newState.counter = state.counter + 1
-            return newState
-        case actionTypes.DECREMENT:
-            return {
-                ...state,
-                counter: state.counter - 1
-            }
-        case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.value
-            }
-        case actionTypes.SUBTRACT:
-            return {
-                ...state,
-                counter: state.counter - action.value
-            }
+// const reducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case actionTypes.INCREMENT:
+//             const newState = Object.assign({}, state)
+//             newState.counter = state.counter + 1
+//             return newState
+//         case actionTypes.DECREMENT:
+//             return {
+//                 ...state,
+//                 counter: state.counter - 1
+//             }
+//         case actionTypes.ADD:
+//             return {
+//                 ...state,
+//                 counter: state.counter + action.value
+//             }
+//         case actionTypes.SUBTRACT:
+//             return {
+//                 ...state,
+//                 counter: state.counter - action.value
+//             }
         
-    }
+//     }
         
-    return state
-}
+//     return state
+// }
 
-export default reducer;
+// export default reducer;
