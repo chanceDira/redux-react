11dd33146033d5dea510b3b68711c91bd01c047c
chanceDira <chancedesire60@gmail.com>
2021-10-02 06:23:41 +0200

    Store implementation

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes'
+import { updateObject } from '../utility'
 
 const initialState = {
     counter: 0
@@ -7,24 +8,28 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.INCREMENT:
-            const newState = Object.assign({}, state)
-            newState.counter = state.counter + 1
-            return newState
+            // const newState = Object.assign({}, state)
+            // newState.counter = state.counter + 1
+            // return newState
+            return updateObject(state, {counter: state.counter + 1})
         case actionTypes.DECREMENT:
-            return {
-                ...state,
-                counter: state.counter - 1
-            }
+            return updateObject(state, {counter: state.counter - 1})
+            // return {
+            //     ...state,
+            //     counter: state.counter - 1
+            // }
         case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.value
-            }
+            return updateObject(state, {counter: state.counter + action.value})
+            // return {
+            //     ...state,
+            //     counter: state.counter + action.value
+            // }
         case actionTypes.SUBTRACT:
-            return {
-                ...state,
-                counter: state.counter - action.value
-            }
+            return updateObject(state, {counter: state.counter - action.value})
+            // return {
+            //     ...state,
+            //     counter: state.counter - action.value
+            // }
         
     }
         
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,27 +1,36 @@
 import * as actionTypes from '../actions/actionTypes'
+import { updateObject  } from '../utility'
 
 const initialState = {
     results: []
+}
+
+const deleteResult = (state, action) => {  //Helper function
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId)
+    return updateObject(state, {results: updatedArray})
 }
 
 const reducer = (state = initialState, action) => { //reducer runs synchronously
     switch (action.type) {
         case actionTypes.STORE_RESULT:
             // You change data here in reducer, if you want
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: action.result * 2})  // value: state.counter //immutable way of adding adding item somehow similar to push
-            }
+            // return {
+            //     ...state,
+            //     results: state.results.concat({id: new Date(), value: action.result * 2})  // value: state.counter //immutable way of adding adding item somehow similar to push
+            // }
+            return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result * 2}) })
         case actionTypes.DELETE_RESULT:
             // const id = 2
             // const newArray = [...state.results]
             // newArray.splice(id, 1)
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId)    //filter return the new array
-            return {
-                ...state,
-                // results: newArray
-                results: updatedArray
-            }
+            // const updatedArray = state.results.filter(result => result.id !== action.resultElId)    //filter return the new array
+            // return {
+            //     ...state,
+            //     // results: newArray
+            //     results: updatedArray
+            // }
+            // return updateObject(state, {results: updatedArray})
+            return deleteResult(state, action)
         
     }
         
